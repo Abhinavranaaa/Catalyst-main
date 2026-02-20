c94b3390ec2879ac5c0ccb7fc7e0fe57207cf494
Abhinavranaaa <abhinav20353@iiitd.ac.in>
2026-02-20 18:42:34 +0530

    changes to remove secrets from settings.py

--- servers/catalyst/catalyst/settings.py
+++ servers/catalyst/catalyst/settings.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv
 from datetime import timedelta
 import os
 import nltk
+from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 NLTK_DATA_PATH = os.path.join(BASE_DIR, 'nltk_data')
@@ -114,17 +115,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'catalyst.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres.qbenybbdxqeaciygkxup",
-        "PASSWORD": "Catalyst@1234#",
-        "PORT": "6543",
-        "HOST": "aws-1-us-east-1.pooler.supabase.com",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "PORT": config("DB_PORT"),
+        "HOST": config("DB_HOST"),
         'OPTIONS': {
             'options': '-c statement_timeout=10000',
             'connect_timeout': 10,
@@ -159,23 +157,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
-#
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:4200",
-#     "https://catalyst-roadmap.vercel.app",
-# ]
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
@@ -208,8 +196,8 @@ EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
-EMAIL_HOST_USER = 'admin@catalystedutech.com'
-EMAIL_HOST_PASSWORD = 'Catalyst@28092025'
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 # Default "from" email address for outgoing emails
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
