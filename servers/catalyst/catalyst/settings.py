afde4c98046c20e80c3aa141e4ffbd0b8315b163
Abhinavranaaa <abhinav20353@iiitd.ac.in>
2026-03-15 23:31:37 +0530

    changes for cors

--- servers/catalyst/catalyst/settings.py
+++ servers/catalyst/catalyst/settings.py
@@ -171,7 +171,7 @@ AUTH_USER_MODEL = "users.User"
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
     "https://catalyst-roadmap.vercel.app",
-    "localhost:4200"
+    "http://localhost:4200"
 ]
 CORS_ALLOW_CREDENTIALS = True
 # change here later on
